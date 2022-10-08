4910c61fa5ccfe49fb52b880567954fc2b5a2f43
Amit Maurya <amitMaurya9669@gmail.com>
2022-10-09 05:11:03 +0530

    Added Climbing Star Problem

---
+++ DSA/DP/Climbing Stars.cpp
@@ -0,0 +1,24 @@
+// Problem Link : https://leetcode.com/problems/climbing-stairs/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() 
+{
+
+  int n=3;
+  
+  int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++)
+  {
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
